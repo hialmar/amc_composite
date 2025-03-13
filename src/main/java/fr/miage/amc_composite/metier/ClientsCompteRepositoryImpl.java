e3abc7dda77f8f7a660c1806b70ade24f1663096
Patrice Torguet <torguet@gmail.com>
2025-03-13 17:33:48 +0100

    Traces erreurs recup client et comptes.

--- src/main/java/fr/miage/amc_composite/metier/ClientsCompteRepositoryImpl.java
+++ src/main/java/fr/miage/amc_composite/metier/ClientsCompteRepositoryImpl.java
@@ -1,6 +1,7 @@
 package fr.miage.amc_composite.metier;
 
 
+import feign.FeignException;
 import fr.miage.amc_composite.clients.ClientClients;
 import fr.miage.amc_composite.clients.ClientComptes;
 import fr.miage.amc_composite.transientObj.Client;
@@ -33,20 +34,32 @@ public class ClientsCompteRepositoryImpl implements ClientsCompteRepository {
 
         try {
             // On récupère 1 objet client
-            Client c = this.clientclients.getClient(idclient, bearerToken);
-            logger.info("On a recue la réponse client : {}", c);
-
-            // On récupère la liste des comptes pour 1 client donné
-            logger.info("On envoie la demande au service compte");
-            List<Compte> cpts = this.clientcomptes.getComptes(c.getId(), bearerToken);
-            logger.info("On a recue la réponse compte : {}", c);
-
+            Client c;
+            try {
+                c = this.clientclients.getClient(idclient, bearerToken);
+                logger.info("On a recue la réponse client : {}", c);
+            } catch (FeignException.NotFound e) {
+                e.printStackTrace();
+                logger.warn(e.getLocalizedMessage());
+                return new ClientWithCompte();
+            }
             // On forge la réponse
             ClientWithCompte cwc = new ClientWithCompte();
             cwc.setId(c.getId());
             cwc.setNom(c.getNom());
             cwc.setPrenom(c.getPrenom());
-            cwc.setComptes(cpts);
+
+            // On récupère la liste des comptes pour 1 client donné
+            logger.info("On envoie la demande au service compte");
+            try {
+                List<Compte> cpts = this.clientcomptes.getComptes(c.getId(), bearerToken);
+                logger.info("On a recue la réponse compte : {}", c);
+                cwc.setComptes(cpts);
+            } catch (FeignException.NotFound e) {
+                e.printStackTrace();
+                logger.warn(e.getLocalizedMessage());
+                return cwc;
+            }
             return cwc;
         } catch(feign.FeignException feignException) {
             logger.warn(feignException.getLocalizedMessage());
